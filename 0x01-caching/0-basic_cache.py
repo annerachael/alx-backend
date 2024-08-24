732bd0b8687f8084543a1e729e6d53ae5c685c56
annerachael <annerwenji@gmail.com>
2024-08-24 04:13:05 -0700

    BasicCache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Basic Cache"""
+
+from base_caching import BaseCaching
+
+class BasicCache(BaseCaching):
+    """Basic Dictionary"""
+
+    def __init__(self):
+        """Initialize BasicCache"""
+
+        super().__init__()
+
+    def put(self, key, item):
+        """insert into cache"""
+
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key, item):
+        """retrieve cache items"""
+
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
+        
+
