845c322337854a95187b5abe2cda068acb14c57d
annerachael <annerwenji@gmail.com>
2024-08-28 08:29:43 -0700

    flask babel

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     """ define our root directory as index"""
-    return "<title> Welcome to Holberton </title> <h1> Hello world </h1>"
+    return render_template('0-index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
